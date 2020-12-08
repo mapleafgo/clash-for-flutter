9b830fbd1fc76869998a893a37c2f577d942bdfe
慕枫 <javakang@qq.com>
2020-12-08 19:06:32 +0800

    调整版本为0.0.4

--- go/cmd/pac-proxy.go
+++ go/cmd/pac-proxy.go
@@ -20,14 +20,14 @@ var proxy = __PROXY__;
 var direct = "DIRECT";
 
 function FindProxyForURL(url, host) {
-	if (isPlainHostName(host) ||
-			shExpMatch(host, "*.local") ||
-			isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
-			isInNet(dnsResolve(host), "172.16.0.0",  "255.240.0.0") ||
-			isInNet(dnsResolve(host), "192.168.0.0",  "255.255.0.0") ||
-			isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0")) {
-		return direct;
-	}
+  if (isPlainHostName(host) ||
+    shExpMatch(host, "*.local") ||
+    isInNet(dnsResolve(host), "10.0.0.0", "255.0.0.0") ||
+    isInNet(dnsResolve(host), "172.16.0.0",  "255.240.0.0") ||
+    isInNet(dnsResolve(host), "192.168.0.0",  "255.255.0.0") ||
+    isInNet(dnsResolve(host), "127.0.0.0", "255.255.255.0")) {
+    return direct;
+  }
 	return proxy + direct;
 }
 `
