d89cd6346ef41085c98497cffaf5d5843b4d716d
mapleafgo <mapleafgo@163.com>
2022-12-09 21:28:16 +0800

    去掉托盘标题，减少占位

--- lib/app/pages/index/index_page.dart
+++ lib/app/pages/index/index_page.dart
@@ -49,7 +49,7 @@ class _IndexPageState extends State<IndexPage>
       Platform.isWindows ? 'assets/icon.ico' : 'assets/logo_64.png',
     );
     trayMenuChange(_config.systemProxy);
-    trayManager.setTitle("ClashForFlutter").catchError((_) {});
+    trayManager.setToolTip("ClashForFlutter").catchError((_) {});
   }
 
   @override
